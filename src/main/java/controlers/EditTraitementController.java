cb3e1567aa257939926c6983e2745ccc938c8612
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-06-01 05:04:23 +0100

    Replace TextField with TextArea for notes in EditTraitementController and FXML view

--- src/main/java/controlers/EditTraitementController.java
+++ src/main/java/controlers/EditTraitementController.java
@@ -16,6 +16,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
@@ -37,7 +38,7 @@ public class EditTraitementController implements Initializable {
     @FXML
     private TextField descriptionField;
     @FXML
-    private TextField notesField;
+    private TextArea notesField;
     @FXML
     private TextField raisonField;
     @FXML
